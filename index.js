1a30b875705f03c5ebf55a862b9322ea95000118
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-06-21 11:18:29 -0400

    change how tokens provider is exported

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { beginNewPlayback, initializeAudioRuntime, setComputedAudio } from './main/src/runtime-business/AudioRuntime.js';
 export { setGetAnimationInfoFunction, setSyllableTime, repaintColors, initiateAnimation, areWeCurrentlyPlayingBack} from './main/src/runtime-business/AnimationRuntime.js';
-export * as MotorMusicTokensProvider from './main/generated-javascript/main/src/typescript/MotorMusicTokensProvider.js';
+export {MotorMusicTokensProvider} from './main/generated-javascript/main/src/typescript/MotorMusicTokensProvider.js';
 export {process} from './main/generated-javascript/main/src/typescript/Compile.js'
 export {DEFAULT_SYLLABLE_TIME} from './main/generated-javascript/main/src/runtime-business/RuntimeConstants.js';
