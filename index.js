6f94a789c638d821e02fb6548f28f718fa0a58ab
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-07-06 20:41:43 -0400

    fix exports to align with new standard

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-export { beginNewPlayback, initializeAudioRuntime, setComputedAudio } from './main/src/runtime-business/AudioRuntime.js';
-export { setGetAnimationInfoFunction, setSyllableTime, repaintColors, initiateAnimation, areWeCurrentlyPlayingBack} from './main/src/runtime-business/AnimationRuntime.js';
+export {initiateMotorMusicRuntime} from "./main/src/runtime-business/MotorMusicRuntime.js";
 export {MotorMusicTokensProvider} from './main/generated-javascript/main/src/typescript/MotorMusicTokensProvider.js';
 export {process} from './main/generated-javascript/main/src/typescript/Compile.js'
 export {DEFAULT_SYLLABLE_TIME} from './main/generated-javascript/main/src/runtime-business/RuntimeConstants.js';
