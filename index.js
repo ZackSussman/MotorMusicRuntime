8fe365d0a321b05829ed810e3ad1ea0c938b65a5
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-07-06 20:44:04 -0400

    fix typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {initiateMotorMusicRuntime} from "./main/src/runtime-business/MotorMusicRuntime.js";
+export {initializeMotorMusicRuntime} from "./main/src/runtime-business/MotorMusicRuntime.js";
 export {MotorMusicTokensProvider} from './main/generated-javascript/main/src/typescript/MotorMusicTokensProvider.js';
 export {process} from './main/generated-javascript/main/src/typescript/Compile.js'
 export {DEFAULT_SYLLABLE_TIME} from './main/generated-javascript/main/src/runtime-business/RuntimeConstants.js';
