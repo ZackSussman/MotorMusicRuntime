d27ebb6ff9fe689196cbc1464020948992176913
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-08-23 19:38:27 -0400

    complete initializations for containment outer layer case

--- main/src/typescript/ParserListeners/Coloring.ts
+++ main/src/typescript/ParserListeners/Coloring.ts
@@ -143,6 +143,9 @@ export class ProgramColoringListener extends MotorMusicParserListener {
         if (this.currentBracesInScope.length === 0) {
             //we are not in any braces, so this containment is on the outside of the whole program
             this.braceFreeSyllableGroupData = new BraceFreeSyllableGroupData(containmentData.syllableRanges, containmentData.ampersandRanges);
+            this.currentBracesInScope.push(new BraceData(
+                this.getRangesFromContainmentCtx(ctx), 0)
+            );
             return;
         }
         
