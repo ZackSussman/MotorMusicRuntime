803dd13098b68a9d9a543af25b57590ef7d07cd7
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-08-23 18:01:53 -0400

    first debug log

--- main/src/runtime-business/AnimationRuntime.js
+++ main/src/runtime-business/AnimationRuntime.js
@@ -191,7 +191,7 @@ export function initializeAnimationRuntime(globalRuntimeData, audioRuntimeData, 
             //update the colors based on the computed animation function 
             function animationRuntime() {
                 const elapsedTime = (audioRuntimeData.audioContext.currentTime - animationRuntimeData.startTime) * 1000;  // Time elapsed in ms
-                //console.log("elapside time is " + elapsedTime);
+                console.log("elapside time is " + elapsedTime);
                 
                 let animationInfo = animationRuntimeData.getAnimationInfoFunction(Math.max(elapsedTime - DELAY_BEFORE_PLAYBACK_START + CORRECTION_FACTOR, 0)); //apply a .1 second shift to align with the delay the audio is forced to have
                 //it gives back undefined once elapside time has gone above what there is actual animation for 
