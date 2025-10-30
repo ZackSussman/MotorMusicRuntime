a9969c89203abe7f66becc2a15760193956d9766
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-10-30 07:34:50 -0400

    add additional log

--- main/src/typescript/ParserListeners/AudioGeneratorListener.ts
+++ main/src/typescript/ParserListeners/AudioGeneratorListener.ts
@@ -173,6 +173,7 @@ export class AudioGeneratorListener extends MotorMusicParserListener {
     enterSyllableGroup = (ctx : SyllableGroupContext) => { 
         console.log("entered syllable group " + ctx.getText() + ", the index is " + this.currentLeafSyllableGroupIndex);
         if (this.areWeCurrentlyInAContainmentGroup) {
+            console.log("were we in a containment group? " + this.areWeCurrentlyInAContainmentGroup);
             return;
         }
         // console.log( ctx.getText());
